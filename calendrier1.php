01553b30649a8bd64b3fc9a47f6093f9cf706c48
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2023-04-11 16:44:47 +0200

    calendrier1 version basique faites

--- calendrier1.php
+++ calendrier1.php
@@ -6,11 +6,11 @@ setlocale(LC_TIME, 'fr_FR.utf8');
 // Les jours de la semaine
 $semaine = ['L', 'M', 'M', 'J', 'V', 'S', 'D'];
 // Le jour courant
-$dateTime = new DateTimeImmutable();
+$dateTime = new DateTimeImmutable('06/01/2023');
 // Nom du mois et année
 $nomMoisEtAnnee = IntlDateFormatter::formatObject($dateTime, 'MMMM yyyy');
 // Numéro du premier jour du mois (1:lundi, 7:dimanche)
-$numeroPremierJourDuMois =  (int) $dateTime->format('N');
+$numeroPremierJourDuMois =  (int) $dateTime->format('N')-1;
 // Nombre de jours dans le mois
 $nombreDeJoursDuMois = (int) $dateTime->format('t');
 
@@ -26,17 +26,39 @@ $html= <<<HTML
         <h1>Calendrier</h1>
         <table>
 HTML;
-$html.="<tr><th colspan='7'>$nomMoisEtAnnee</th></tr>\n";
+$html.="\n<tr><th colspan='7'>$nomMoisEtAnnee</th></tr>\n";
 $html.="<tr>";
 foreach($semaine as $day){
     $html.="<th>$day</th>";
 }
+$html.="</tr>\n<tr>";
+$nbCases=$nombreDeJoursDuMois+$numeroPremierJourDuMois;
+$nbLigne=round(($nbCases+1)/7,0);
+$MonthDay=1;
+$CellNumber=0;
+while($CellNumber<$numeroPremierJourDuMois){
+    $html.="<td></td>";
+    $CellNumber++;
+}
+while($CellNumber<$nbCases){
+    $dayN=$CellNumber%7;
+    $class="";
+    if($dayN==5 || $dayN==6){
+        $class=" class='weekend'";
+    }
+    $html.="<td$class>$MonthDay</td>";
+    if($dayN==6){
+        $html.="</tr>\n<tr>";
+    }
+    $CellNumber++;
+    $MonthDay++;
+}
+while($CellNumber<$nbLigne*7){
+    $html.="<td></td>";
+    $CellNumber++;
+}
 $html.="</tr>\n";
 
-$nbLigne=round(($nombreDeJoursDuMois+$numeroPremierJourDuMois)/7,0);
-for($i=1; $i<$nbLigne; $i++){
-
-}
 
 
 
